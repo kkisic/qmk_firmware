803d1a1ed4a6e8c9848294fb5c5a068be7d4f044
kkisic <amareamo.pxhxc@gmail.com>
2020-04-06 01:26:32 +0900

    fix equinox

--- keyboards/ai03/equinox/keymaps/kkisic/keymap.c
+++ keyboards/ai03/equinox/keymaps/kkisic/keymap.c
@@ -21,27 +21,98 @@
     K300, K301, K302,     K304,       K306,       K308,   K309, K310, K311  \
 */
 
+#define _QWERTY 0
+#define _LOWER 3
+#define _RAISE 4
+#define _ADJUST 16
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST
+};
+
 // なぜかGUIとLCTLが逆なので入れ替える
 #define KC_CTLESC CTL_T(KC_ESC)
 #define KC_ALTBSC ALT_T(KC_BSPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_all( /* Base */
+  [_QWERTY] = LAYOUT_all( /* Base */
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, 
     KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    XXXXXXX, KC_SCLN, 
     KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, 
-    KC_LCTL, XXXXXXX, KC_CTLESC,        MO(1),           KC_SPC,         MO(2),    KC_ALTBSC, XXXXXXX, KC_ENT
+    KC_LGUI, XXXXXXX, KC_CTLESC,        LOWER,            KC_SPC,         RAISE,     KC_ALTBSC, XXXXXXX, KC_ENT
   ),
-  [1] = LAYOUT_all(
+  [_LOWER] = LAYOUT_all(
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_RBRC, 
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    XXXXXXX, KC_0, 
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
     _______, XXXXXXX, _______,          _______,          _______,     _______,      _______, XXXXXXX, _______
   ),
-  [2] = LAYOUT_all(
+  [_RAISE] = LAYOUT_all(
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_RBRC, 
     _______, KC_LBRC, KC_RBRC, KC_GRV,  KC_MINS, KC_EQL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_ENT, 
     _______, XXXXXXX, KC_LCBR, KC_RCBR, KC_TILD, KC_UNDS, KC_PLUS, KC_QUOT, KC_DQUO, KC_BSLS, KC_PIPE, XXXXXXX, 
-    _______, XXXXXXX, _______,          _______,          _______,     _______,      _______, XXXXXXX, RESET
+    _______, XXXXXXX, _______,          _______,          _______,     _______,      _______, XXXXXXX, _______
+  ),
+  [_ADJUST] = LAYOUT_all(
+    RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, 
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+    _______, XXXXXXX, _______,          _______,          _______,     _______,      _______, XXXXXXX, _______
   )
 };
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
+  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
+    layer_on(layer3);
+  } else {
+    layer_off(layer3);
+  }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case ADJUST:
+        if (record->event.pressed) {
+          layer_on(_ADJUST);
+        } else {
+          layer_off(_ADJUST);
+        }
+        return false;
+        break;
+  }
+  return true;
+}
